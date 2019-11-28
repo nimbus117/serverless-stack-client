41ecae172bfe4359a4e8903fb8ea01d4e9f46272
nimbus117 <mail@nimbus117.co.uk>
2019-11-28 17:57:59 +0000

    setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    angular: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+    // sourceType: 'script'
+  },
+  plugins: ['prettier', 'react'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'react/jsx-filename-extension': ['off']
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  overrides: [
+    Object.assign(
+      {
+        files: ['**/*.test.js'],
+        env: { jest: true },
+        plugins: ['jest']
+      },
+      require('eslint-plugin-jest').configs.recommended
+    )
+  ]
+};
