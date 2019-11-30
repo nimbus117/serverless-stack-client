e696780a3964764f63f96f16e4a57ac351254c9c
nimbus117 <mail@nimbus117.co.uk>
2019-11-30 15:49:44 +0000

    Adding a Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   STRIPE_KEY: 'pk_test_tHrq4wGpXuzsyVLb2Ptglomb00ZueadHzK',
   s3: {
     REGION: 'eu-west-1',
@@ -15,4 +14,31 @@ export default {
     APP_CLIENT_ID: '360guga9uttidrqk0qt8s79k0p',
     IDENTITY_POOL_ID: 'eu-west-1:52d5a9f1-6d47-4353-ae32-0bb172fe55f9'
   }
+};
+
+const prod = {
+  STRIPE_KEY: 'pk_test_tHrq4wGpXuzsyVLb2Ptglomb00ZueadHzK',
+  s3: {
+    REGION: 'eu-west-1',
+    BUCKET: 'notes-app-api-prod-attachmentsbucket-a6qe1i20s6t6'
+  },
+  apiGateway: {
+    REGION: 'eu-west-1',
+    URL: 'https://xqic6hby37.execute-api.eu-west-1.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'eu-west-1',
+    USER_POOL_ID: 'eu-west-1_GMresltya',
+    APP_CLIENT_ID: '5lv6ldfkvn0bokjq2rnh3krtfi',
+    IDENTITY_POOL_ID: 'eu-west-1:cdad69da-beb2-48fd-b736-f3199e4ff112'
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod' ? prod : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
