6da848e54ed38e86167862280837809a4f47e989
nimbus117 <mail@nimbus117.co.uk>
2019-11-30 13:14:38 +0000

    update config

--- src/config.js
+++ src/config.js
@@ -3,16 +3,16 @@ export default {
   STRIPE_KEY: 'pk_test_tHrq4wGpXuzsyVLb2Ptglomb00ZueadHzK',
   s3: {
     REGION: 'eu-west-1',
-    BUCKET: 'n117-serverless-notes-app-uploads'
+    BUCKET: 'notes-app-api-dev-attachmentsbucket-1q0clb241bo7s'
   },
   apiGateway: {
     REGION: 'eu-west-1',
-    URL: 'https://j2480ebprh.execute-api.eu-west-1.amazonaws.com/prod'
+    URL: 'https://u48hcugfie.execute-api.eu-west-1.amazonaws.com/dev'
   },
   cognito: {
     REGION: 'eu-west-1',
-    USER_POOL_ID: 'eu-west-1_lcWGak1Zn',
-    APP_CLIENT_ID: '3nnun8dsp22tqpi6jnubuekh35',
-    IDENTITY_POOL_ID: 'eu-west-1:130db55e-8e34-4f3f-96be-5e967c100a50'
+    USER_POOL_ID: 'eu-west-1_yd120jnfn',
+    APP_CLIENT_ID: '360guga9uttidrqk0qt8s79k0p',
+    IDENTITY_POOL_ID: 'eu-west-1:52d5a9f1-6d47-4353-ae32-0bb172fe55f9'
   }
 };
